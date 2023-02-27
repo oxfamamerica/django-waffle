1507d64cc3009b79c8a2d495cce51b993b461033
Jessica Ingari <jingari@oxfamamerica.org>
2023-02-27 12:01:45 -0500

    add wagtail hooks

---
+++ waffle/wagtail_hooks.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.dispatch import receiver
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    ModelAdminGroup,
+    modeladmin_register,
+)
+from waffle.models import AbstractBaseFlag
+
+
+class WaffleAdmin(ModelAdmin):
+    model = AbstractBaseFlag 
+    menu_label = "Waffle Flags"  
+    menu_icon = "pick" 
+    menu_order = 200 
+    list_display = ("name","everyone", "percent", "testing", "rollout")
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+modeladmin_register(WaffleAdmin)
+
+
